e2ca6d67f3fe8245be4a6727a66d418b877e129c
Yousef Barakat <yousefhany19992@gmail.com>
2023-06-20 22:58:31 +0200

    update

--- auctions/models.py
+++ auctions/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
-    username = models.CharField(unique=True)
+    username = models.CharField(max_length=1024, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField()
+    password = models.CharField(max_length=1024)
     watchlist = models.ManyToManyField(
         'auctions', blank=True, related_name="watchlist")
     
