8a667ceabe459f4058dcb51f51d55a601592b657
Yousef Barakat <yousefhany19992@gmail.com>
2023-06-21 18:31:46 +0200

    -

--- commerce/settings.py
+++ commerce/settings.py
@@ -132,4 +132,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'auctions/static/'
+STATIC_URL = 'auctions/static/' # For production
+# STATIC_URL = '/auctions/static/' For local development
